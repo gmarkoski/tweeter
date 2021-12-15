7aa1f07b6fbcb23dc151fe61e93a717dcfcd6038
gmarkoski <gary_markoski@hotmail.com>
2021-12-14 21:10:04 -0700

    with counter functionality

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  const $tBox = $("#tweet-text");
+  $tBox.on("input", function() {
+    const textInput = $(this).val();
+    $(this).parent().find('.counter').text(140 - textInput.length);
+    if (textInput.length > 140) {
+      $(this).parent().find('.counter').addClass("negative-num");
+    } else { 
+      $(this).parent().find('.counter').removeClass("negative-num");
+    }    
+  })
+});
+
